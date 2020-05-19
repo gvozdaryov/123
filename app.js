721b964e16fd34c8034c1d132e4a94b3a316bf90
Vlad <gvozdarevv9@gmail.com>
2020-05-19 15:58:28 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+let serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(2000);
